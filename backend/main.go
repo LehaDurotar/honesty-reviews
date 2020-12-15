cdfce6fc04c5c6f8f322142ece91dd2c9730e76a
LehaDurotar <hwick1396@gmail.com>
2020-12-15 16:49:18 +0300

    Initial commit

---
+++ backend/main.go
@@ -0,0 +1,1 @@
+package honesty_reviews
---
+++ frontend/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Navbar />
+    <main>
+      <div class="contaier">
+        <nuxt />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+export default {
+  components: {
+    Navbar,
+  },
+}
+</script>
+
+<style></style>
